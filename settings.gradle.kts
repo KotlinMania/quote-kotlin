3027f7bc9a7b2e69aa27b41dd9e25603c75544c4
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-14 16:47:05 -0700

    Recovery anchor for unreachable tree 1bb0347141accc759e2f889d731cf98cd8c7633e

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
     plugins { kotlin("multiplatform") version "2.3.21" }
 }
 
-plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0" }
 
 dependencyResolutionManagement {
     repositories {
@@ -17,3 +17,12 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "quote-kotlin"
+
+val procMacro2Local = file("../proc-macro2-kotlin")
+if (procMacro2Local.exists()) {
+    includeBuild(procMacro2Local) {
+        dependencySubstitution {
+            substitute(module("io.github.kotlinmania:proc-macro2-kotlin")).using(project(":"))
+        }
+    }
+}
