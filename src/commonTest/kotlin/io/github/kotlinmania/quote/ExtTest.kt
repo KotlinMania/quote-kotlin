9469ff5f877b05f2ccb3ca76f950dbefee74fb5a
Sydney Renee <sydney@solace.ofharmony.ai>
2026-08-24 09:40:33 -0700

    test(quote): add ExtTest suite for appendSeparated and appendTerminated

--- src/commonTest/kotlin/io/github/kotlinmania/quote/ExtTest.kt
+++ src/commonTest/kotlin/io/github/kotlinmania/quote/ExtTest.kt
@@ -1,15 +1,13 @@
 // port-lint: tests tests/test.rs
 package io.github.kotlinmania.quote
 
-import io.github.kotlinmania.procmacro2.Ident
-import io.github.kotlinmania.procmacro2.Span
 import io.github.kotlinmania.procmacro2.TokenStream
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-private class TokenWrapper(private val text: String) : ToTokens {
+private class StreamWrapper(private val stream: TokenStream) : ToTokens {
     override fun toTokens(tokens: TokenStream) {
-        tokens.append(Ident.new(text, Span.callSite()))
+        stream.toTokens(tokens)
     }
 }
 
@@ -17,15 +15,15 @@ class ExtTest {
     @Test
     fun testAppendSeparatedAndTerminated() {
         val stream1 = TokenStream.new()
-        val id1 = TokenWrapper("a")
-        val id2 = TokenWrapper("b")
-        val comma = TokenWrapper(",")
+        val id1 = StreamWrapper(quote("a"))
+        val id2 = StreamWrapper(quote("b"))
+        val comma = StreamWrapper(quote(","))
 
         stream1.appendSeparated(listOf(id1, id2), comma)
         assertEquals("a , b", stream1.toString())
 
         val stream2 = TokenStream.new()
-        val semi = TokenWrapper(";")
+        val semi = StreamWrapper(quote(";"))
         stream2.appendTerminated(listOf(id1, id2), semi)
         assertEquals("a ; b ;", stream2.toString())
     }
